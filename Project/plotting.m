129ffe257af359f5e149ecec23e46a7f4821c9af
Yves Greatti <ygrepo@gmail.com>
2025-04-26 13:47:46 -0400

    Updates

--- Project/plotting.m
+++ Project/plotting.m
@@ -5,7 +5,7 @@ ktilde_c = (1 + alpha) * kc;
 
 % Range of ECM stiffness (km)
 km = logspace(-2, 2, 500); % from 0.01 to 100 (log scale)
-
+%km = 0:10:100;
 % Compute fa/f0
 fa_over_f0 = alpha .* km ./ (ktilde_c + km);
 
@@ -13,9 +13,9 @@ fa_over_f0 = alpha .* km ./ (ktilde_c + km);
 figure;
 plot(km, fa_over_f0, 'LineWidth', 2);
 set(gca, 'XScale', 'log');
-xlabel('Matrix Stiffness k_m (log scale)', 'FontSize', 12);
-ylabel('Normalized Active Force f^a / f^0', 'FontSize', 12);
-title('Saturation of Active Force with ECM Stiffness', 'FontSize', 14);
+xlabel('Matrix Stiffness k_m (log scale)', 'FontWeight', 'bold', 'FontSize', 24);
+ylabel('Normalized Active Force f^a / f^0', 'FontWeight', 'bold', 'FontSize', 24);
+title('Saturation of Active Force with ECM Stiffness', 'FontWeight', 'bold', 'FontSize', 24);
 grid on;
 hold on;
 yline(alpha, '--r', ['Saturation level (\alpha = ', num2str(alpha), ')']);
